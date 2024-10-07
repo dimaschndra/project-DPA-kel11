c3785524ca2d866c152612bfef84ff2f95350c17
dimaschndra <dimaskurniachandra70@gmail.com>
2024-10-07 20:06:50 +0700

    Update DPA.cpp

--- DPA.cpp
+++ DPA.cpp
@@ -1,1 +1,11 @@
+#include <iostream>
+#include <cmath>
+using namespace std;
 
+double volum_segitiga(double alas, double tinggi){
+    return (0,5*alas*tinggi );
+}
+
+int main(){
+ cout << volum_segitiga(3,3);
+}
