4c20ea3fb1c405e69fa4707ffcb77a66a869d1e2
dimaschndra <dimaskurniachandra70@gmail.com>
2024-10-08 21:18:11 +0700

    Update DPA.cpp

--- DPA.cpp
+++ DPA.cpp
@@ -1,14 +1,21 @@
 #include <iostream>
 #include <cmath>
+
 using namespace std;
 
-double hitungJarak(double x1, double y1, double x2, double y2) {
-    return sqrt(pow(x2 - x1, 2) + pow(y2 - y1, 2));
-}
-int main() {
-    int jumlahLokasi;
-    cout << "Masukkan jumlah lokasi: ";
-    cin >> jumlahLokasi;
-return 0;
+    double HitungJarakTempuh(double X1, double Y1, double X2, double Y2) {
+    return sqrt(pow(X2 - X1, 2) + pow(Y1 - Y2, 2));
 }
 
+int main(){
+    int JumlahLokasi;
+    cout << "Masukan Jumlah lokasi yang dicari : ";
+    cin >> JumlahLokasi;
+
+   double X[JumlahLokasi], Y[JumlahLokasi];
+   for (int i = 0; i < JumlahLokasi; i++) {
+   cout << "Masukan kordinat lokasi : " << i + 1 << " (X Y) ";
+   cin >> X[i] >> Y[i];
+}
+int Urutan[JumlahLokasi];
+bool SudahDiKunjungi[JumlahLokasi] = {false};
