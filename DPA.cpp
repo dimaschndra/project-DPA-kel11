76d0423a50454a5c3df2e73bd8a80befb23f7056
Putrifzhrhma <putri.fauziah206@gmail.com>
2024-10-08 21:21:54 +0700

    Update DPA_PutriFr.cpp

--- DPA.cpp
+++ DPA.cpp
@@ -44,3 +44,23 @@ bool SudahDiKunjungi[JumlahLokasi] = {false};
         SudahDiKunjungi[LokasiBerikutnya] = true;
         TotalJarak += JarakTerdekat;
     }
+    
+TotalJarak += HitungJarakTempuh(X[Urutan[JumlahLokasi - 1]], Y[Urutan[JumlahLokasi - 1]], X[0], Y[0]);
+
+    char pilihan;
+    cout << "Pilih satuan jarak (m untuk meter, km untuk kilometer): ";
+    cin >> pilihan;
+
+    if (pilihan == 'k' || pilihan == 'K') {
+        TotalJarak /= 1000.0;
+    }
+
+    cout << " Urutan kunjungan lokasi: ";
+    for (int i = 0; i < JumlahLokasi; ++i) {
+        cout << Urutan[i] + 1 << " ";
+    }
+    cout << "\nTotal jarak yang ditempuh: " << TotalJarak << " " 
+         << (pilihan == 'k' || pilihan == 'K' ? "km" : "meter") << endl;
+
+    return 0;
+}
